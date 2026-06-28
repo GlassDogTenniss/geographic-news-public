0a188937accc2b975fae4f10bb592f05f696505c
GlassDogTenniss <67187804+GlassDogTenniss@users.noreply.github.com>
2026-06-28 20:13:28 +0900

    Refresh unknown place app script version

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-import('./unknown-place-label-patch.js?v=20260628-1').then(() => import('./app-kunlun.js?v=20260628-1'));
+import('./unknown-place-label-patch.js?v=20260628-1').then(() => import('./app-kunlun.js?v=20260628-2'));

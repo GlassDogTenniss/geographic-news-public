82016bef603677c6b338d69db23f0ac05d282722
GlassDogTenniss <67187804+GlassDogTenniss@users.noreply.github.com>
2026-06-30 23:22:15 +0900

    Bump red marker cache version

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-import('./unknown-place-label-patch.js?v=20260628-1').then(() => import('./app-kunlun.js?v=20260630-1'));
+import('./unknown-place-label-patch.js?v=20260628-1').then(() => import('./app-kunlun.js?v=20260630-2'));
